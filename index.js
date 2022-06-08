dd3694aa5411892e54417efdf70f7aefd341c619
svbutko <svbutko@hotmail.com>
2022-06-08 17:23:03 +0300

    Remove preview from usage

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { requireNativeComponent, View, Platform } from "react-native";
+import {requireNativeComponent} from "react-native";
 
 const NativeContextMenu = requireNativeComponent("ContextMenu", null);
 
-const ContextMenu = (props) => {
+const ContextMenu = ({children, ...props}) => {
   return (
-    <NativeContextMenu {...props}>
+    <NativeContextMenu {props}>
       {props.children}
-      {props.preview != null && Platform.OS === 'ios' ? (
-        <View nativeID="ContextMenuPreview">{props.preview}</View>
-      ) : null}
     </NativeContextMenu>
   );
 };
--- ios/ContextMenuView.m
+++ ios/ContextMenuView.m
@@ -17,28 +17,22 @@
 
 @implementation ContextMenuView {
   BOOL _cancelled;
-  UIView *_customView;
 }
 
 - (id) init {
     self = [super init];
-    
+
     if (@available(iOS 14.0, *)) {
         self.contextMenuInteractionEnabled = true;
     } else {
         // Fallback on earlier versions
     }
-    
+
     return self;
 }
 
 - (void)insertReactSubview:(UIView *)subview atIndex:(NSInteger)atIndex
 {
-  if ([subview.nativeID isEqualToString:@"ContextMenuPreview"]) {
-    _customView = subview;
-    return;
-  }
-
   [super insertReactSubview:subview atIndex:atIndex];
 
   if (@available(iOS 13.0, *)) {
@@ -67,12 +61,7 @@
 - (nullable UIContextMenuConfiguration *)contextMenuInteraction:(nonnull UIContextMenuInteraction *)interaction configurationForMenuAtLocation:(CGPoint)location API_AVAILABLE(ios(13.0)) {
   return [UIContextMenuConfiguration
           configurationWithIdentifier:nil
-          previewProvider:_customView == nil ? nil : ^(){
-            UIViewController* viewController = [[UIViewController alloc] init];
-            viewController.preferredContentSize = self->_customView.frame.size;
-            viewController.view = self->_customView;
-            return viewController;
-          }
+          previewProvider:nil
           actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
             NSMutableArray* actions = [[NSMutableArray alloc] init];
 
